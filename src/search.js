a791f0208b9cd943b4e5b50a68155936f6b29010
ninevra <ninevra@gmail.com>
2020-05-11 12:51:14 -0700

    Move query language to own module

--- src/search.js
+++ src/search.js
@@ -1,65 +1,12 @@
 import lunr from 'lunr';
-import parsimmon from 'parsimmon';
+import queryLang from './query-grammar.js';
 
 async function loadIndex() {
   // TODO: handle fetch errors
   return lunr.Index.load(await (await fetch('/lunr-index.json')).json());
 }
 function parseSearch(query, cards) {
-  const textOperator = (parser) => (
-    // Require whitespace or parentheses after e.g. and, or, not
-    // Prevents "nota" from parsing as "not a"
-    // Also allow whitespace before and after
-    parser.lookahead(parsimmon.regexp(/[()\s]/)).trim(parsimmon.optWhitespace)
-  );
-  const parsers = {
-    colon: () => parsimmon.string(':'),
-    gt: () => parsimmon.string('>'),
-    ge: () => parsimmon.string('>='),
-    eq: () => parsimmon.string('='),
-    le: () => parsimmon.string('<='),
-    lt: () => parsimmon.string('<'),
-    and: () => textOperator(parsimmon.string('and')),
-    or: () => textOperator(parsimmon.string('or')),
-    not: () => textOperator(parsimmon.string('not')),
-    lparen: () => parsimmon.string('('),
-    rparen: () => parsimmon.string(')'),
-    word: () => parsimmon.regexp(/[^"<>=:()\s]+/),
-    quoted: () => parsimmon.regexp(/"[^"]*"/),
-    separator: (l) => parsimmon.alt(l.ge, l.le, l.gt, l.lt, l.eq, l.colon),
-    value: (l) => parsimmon.alt(l.word, l.quoted),
-    term: (l) => parsimmon.alt(
-      parsimmon.seq(l.word, l.separator, l.value),
-      l.value
-    ),
-    basic: (l) => parsimmon.alt(
-      l.term,
-      l.expression.wrap(l.lparen, l.rparen)
-    ),
-    negation: (l) => parsimmon.alt(
-      parsimmon.seq(l.not, l.negation),
-      l.basic
-    ),
-    conjunction: (l) => parsimmon.alt(
-      parsimmon.seq(l.negation, l.and, l.conjunction),
-      l.negation
-    ),
-    disjunction: (l) => parsimmon.alt(
-      parsimmon.seq(l.conjunction, l.or, l.disjunction),
-      l.conjunction
-    ),
-    listConjunction: (l) => parsimmon.alt(
-      parsimmon.seq(
-        l.disjunction,
-        parsimmon.of('&').trim(parsimmon.optWhitespace),
-        l.listConjunction
-      ),
-      l.disjunction
-    ),
-    expression: (l) => l.listConjunction
-  }
-
-  console.log(parsimmon.createLanguage(parsers).expression.parse(getSearchString()));
+  console.log(queryLang.expression.parse(getSearchString()));
 }
 (async () => {
   const [idx, allCards] = await Promise.all([loadIndex(), fetch('/all-cards.json').then(r => r.json())]);
