200ef0c70874e23aa5a0cb5d9a2f0e02b2eada63
ninevra <ninevra@gmail.com>
2020-05-21 01:16:49 -0700

    Minify the search.js bundle and write sourcemaps
    Not for any particular reason.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,16 @@
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/search.js',
   output: {
     file: 'static/search.js',
-    format: 'iife'
+    format: 'iife',
+    sourcemap: true
   },
   plugins: [
-    nodeResolve(), commonjs(), json() 
+    nodeResolve(), commonjs(), json(), terser()
   ]
 }
