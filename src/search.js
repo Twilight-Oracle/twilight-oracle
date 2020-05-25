17056f60c29053098f6ec5c2288e378bd12c9269
ninevra <ninevra@gmail.com>
2020-05-24 20:21:17 -0700

    Remove lunr index from search.js bundle

--- src/search.js
+++ src/search.js
@@ -1,15 +1,10 @@
-import lunr from 'lunr';
 import allCards from '../static/all-cards.json';
-import jsonIndex from '../static/lunr-index.json';
 import queryLang from './query-grammar.js';
 
 // TODO: may no longer need to be async
 (async () => {
   // TODO: is DOMContentLoaded needed here
-  const idx = lunr.Index.load(jsonIndex);
-  console.log(idx);
   const resultsElem = document.getElementById('search-results');
-
   const parseResult = queryLang.expression.parse(getSearchString());
   if (parseResult.status) {
     const ast = parseResult.value;
@@ -22,21 +17,6 @@ import queryLang from './query-grammar.js';
   } else {
     console.error('failed to parse', parseResult);
   }
-
-  // try {
-  //   const results = idx.search(getSearchString());
-  //   console.log(results);
-  //   for (let result of results) {
-  //     const resultElem = createResultElem(result.ref, allCards[result.ref]);
-  //     resultsElem.appendChild(resultElem);
-  //   }
-  // } catch (e) {
-  //   if (e instanceof lunr.QueryParseError) {
-  //     console.warn('caught a QueryParseError', e);
-  //   } else {
-  //     throw e;
-  //   }
-  // }
 })();
 function getSearchString() {
   return new URLSearchParams(location.search).get('q');
